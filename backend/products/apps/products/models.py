26ccac7fd79b6113e3b9133ab9ba7652365f6d35
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-01-08 03:56:55 +0000

    [products-service]: Creacion de modelos preliminares de las apps base y products

--- backend/products/apps/products/models.py
+++ backend/products/apps/products/models.py
@@ -1,91 +1,52 @@
 from django.db import models
+
 from simple_history.models import HistoricalRecords
 from apps.base.models import BaseModel
 
 
 class MeasureUnit(BaseModel):
-    description = models.CharField('Descripción', max_length=50, unique=True, blank=False, null=False)
-    historical = HistoricalRecords()
+    name = models.CharField('Unidad de medida', max_length=20, unique=True)
 
-    @property
-    def _history_user(self):
-        return self.changed_by
-    
-    @_history_user.setter
-    def _history_user(self,value):
-        self.changed_by = value
-
-    
     class Meta:
         verbose_name = 'Unidad de Medida'
-        verbose_name_plural = 'Unidades de Medida'
+        verbose_name_plural = 'Unidades de medida' 
+
+
+    history = HistoricalRecords()
 
     def __str__(self):
-        return self.description
+        return self.name
 
 
-class CategoryProduct(BaseModel):
-    description = models.CharField('Descripción', max_length=50, unique=True, blank=False, null=False)
-    historical = HistoricalRecords()
 
-    @property
-    def _history_user(self):
-        return self.changed_by
-    
-    @_history_user.setter
-    def _history_user(self,value):
-        self.changed_by = value
-
-    
-    class Meta:
-        verbose_name = 'Categoría de Producto'
-        verbose_name_plural = 'Categorías de Productos'
-
-    def __str__(self):
-        return self.description
-
-
-class Indicator(BaseModel):
-    discount_value = models.PositiveBigIntegerField(default=0)
-    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Indicador de Ofertas')
-    historical = HistoricalRecords()
-
-    @property
-    def _history_user(self):
-        return self.changed_by
-    
-    @_history_user.setter
-    def _history_user(self,value):
-        self.changed_by = value
+class Category(BaseModel):
+    name = models.CharField('Categoría', max_length=50, unique=True)
 
     class Meta:
-        verbose_name = 'Indicador de Oferta'
-        verbose_name_plural = 'Indicadores de Ofertas'
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    history = HistoricalRecords()
 
     def __str__(self):
-        return f'Oferta de la categoría {self.category_product}: {self.discount_value}%'
+        return self.name
+    
 
 
 class Product(BaseModel):
-    name = models.CharField('Nombre de Producto', max_length=150, unique=True, blank=False, null=False)
-    description = models.TextField('Descripción del Producto', unique=True, blank=False, null=False)
-    stock = models.PositiveIntegerField('Stock', default=0)
-    image = models.ImageField('Imágen del producto', upload_to='products/', blank=True, null=True)
-    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, verbose_name='Unidad de Medida', null=True)
-    category_product = models.ForeignKey(CategoryProduct, on_delete=models.CASCADE, verbose_name='Categoría de Producto', related_name='productos', null=True)
-    historical = HistoricalRecords()
+    name = models.CharField('Nombre producto', max_length=50)
+    description = models.TextField('Descripción')
+    price = models.DecimalField('Precio', max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='products', verbose_name='Categoría del producto')
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Unidad de medida del producto')
+    image = models.ImageField('Imágen del productp',upload_to='products/' , blank=True, null=True)
 
-    @property
-    def _history_user(self):
-        return self.changed_by
-    
-    @_history_user.setter
-    def _history_user(self,value):
-        self.changed_by = value
 
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
 
+    history = HistoricalRecords()
+
     def __str__(self):
-        return f'Producto: {self.name}'
+        return self.name
