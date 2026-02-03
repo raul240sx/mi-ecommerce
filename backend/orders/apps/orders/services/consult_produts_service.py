0131753b8e1e6fa1187138f5fa80315973e727dc
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-02-03 23:55:52 +0000

    [orders_service]: Archivo de servicio de consulta de productos completado

--- backend/orders/apps/orders/services/consult_produts_service.py
+++ backend/orders/apps/orders/services/consult_produts_service.py
@@ -1,8 +1,11 @@
 import requests
 
 from django.conf import settings
+from django.db import transaction
 
 from rest_framework.exceptions import ValidationError
+
+from apps.orders.models import Order, OrderDetail
 
 
 
@@ -24,6 +27,8 @@ def validate_item_list(expected_items, recieved_items):
 def validate_stock_items(expected_items, recieved_items):
 
     total_amount = 0
+
+    validated_order_items = {}
     
     for item in expected_items:
         recieved_item = recieved_items.get(str(item['product_id']))
@@ -32,12 +37,46 @@ def validate_stock_items(expected_items, recieved_items):
             raise ValidationError(f'Stock insuficiente del producto id {item['product_id']}')
         
         total_amount += (recieved_item['price'] * item['quantity']) 
+
+        new_item = {
+            'id':item['product_id'],
+            'quantity':item['quantity'],
+            'unit_price':recieved_item['price']
+        }
+
+        validated_order_items[item['product_id']] = new_item
     
-    return total_amount
+    return total_amount, validated_order_items
 
 
 
-def validate_and_get_products_info(validated_order_items:list):
+
+def create_order_and_detail(user_id, total_amount, validated_order_items):
+
+    with transaction.atomic():
+
+        order = Order.objects.create(user_id=user_id, total_amount=total_amount)
+
+        details = []
+
+        for item in validated_order_items:
+
+            new_detail = OrderDetail(
+                product_id = item['id'],
+                quantity = item['quantity'],
+                unit_price = item['unit_price'],
+                order = order
+            )
+
+            details.append(new_detail)
+
+        OrderDetail.objects.bulk_create(details)
+
+        return order
+
+
+
+def validate_and_get_products_info(order_items, user_id):
 
     header = {
         'X-Internal_Service-Key':internal_key,
@@ -48,7 +87,7 @@ def validate_and_get_products_info(validated_order_items:list):
 
     products_url
 
-    for item in validated_order_items:
+    for item in order_items:
         items_id.append(item['product_id'])
 
     query_params = {'ids':','.join(map(str, items_id))}
@@ -62,15 +101,12 @@ def validate_and_get_products_info(validated_order_items:list):
             
             validate_item_list(items_id, items_info)
 
-            total_amount = validate_stock_items(validated_order_items, items_info)
+            total_amount, validated_order_items = validate_stock_items(order_items, items_info)
 
 
+            order = create_order_and_detail(user_id, total_amount, validated_order_items)
 
-            return {
-                'products_detail':items_info,
-                'total_amount':total_amount
-                }
-
+            return order
         
         else:
             raise ValidationError('Fallo en peticion HTTP')
