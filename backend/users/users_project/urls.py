b6bd53067daa5303e235f121b4e1a383e14a5e05
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-02-10 22:45:36 +0000

    Configuraciones finales antes de pasar a pruebas de produccion

--- backend/users/users_project/urls.py
+++ backend/users/users_project/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('users/', include('apps.users.urls'))
+    path('users-api/', include('apps.users.urls'))
 
 ]
 
