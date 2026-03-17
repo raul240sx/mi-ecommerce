6ad7cf7d2f4b5a342b2e4bcd9d4c7018fa51dbfe
raul.ramirez1401 <raul.ramirez1401@gmail.com>
2026-03-17 01:08:53 +0000

    [orders-service]: Se añade el campo image_url al serializador order_detail_serializer.py

--- backend/orders/apps/orders/serializers/order_detail_serializer.py
+++ backend/orders/apps/orders/serializers/order_detail_serializer.py
@@ -6,8 +6,8 @@ from apps.orders.models import OrderDetail
 class OrderDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderDetail
-        fields = ['product_id', 'product_title', 'quantity', 'unit_price']
-        read_only_fields = ['product_title', 'unit_price']
+        fields = ['product_id', 'product_title', 'quantity', 'unit_price', 'image_url']
+        read_only_fields = ['product_title', 'unit_price', 'image_url']
         extra_kwargs = {
             'quantity': {'min_value':1},
         }
